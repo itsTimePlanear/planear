410892b7e10d9e221b4353b3e2d9d0742e028c26
links5925 <kilol0@naver.com>
2024-04-27 23:16:32 +0900

    appbar 수정 - UI확정 ver

--- lib/screen/main_screen/main_screen.dart
+++ lib/screen/main_screen/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:planear/riverpod/mainpage_rierpod.dart';
+import 'package:planear/screen/avatar_screen/main_avatar_screen.dart';
 import 'package:planear/widgets/custom_appbar.dart';
 
 class MainScreen extends ConsumerStatefulWidget {
@@ -16,14 +17,17 @@ class _MainScreenState extends ConsumerState<MainScreen> {
     final idxState = ref.watch(bottomChangeStateNotifierProvider);
     final List<Widget> widgetOptions = <Widget>[
       Container(), //일정
-      Container(), //내 아바타
+      const MainAvatarScreen(), //내 아바타
     ];
-    return Scaffold(
-      backgroundColor: const Color(0xFFF4F4F4),
-      appBar: const PreferredSize(
-          preferredSize: Size.fromHeight(80), child: MainAppBar()),
-      body: SafeArea(
-        child: widgetOptions.elementAt(idxState),
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        backgroundColor: const Color(0xFFF4F4F4),
+        appBar: const PreferredSize(
+            preferredSize: Size.fromHeight(60), child: MainAppBar()),
+        body: SafeArea(
+          child: widgetOptions.elementAt(idxState),
+        ),
       ),
     );
   }
--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:planear/riverpod/mainpage_rierpod.dart';
 
@@ -14,42 +15,53 @@ class _MainAppBarState extends ConsumerState<MainAppBar> {
   @override
   Widget build(BuildContext context) {
     final idxRead = ref.read(bottomChangeStateNotifierProvider.notifier);
-    return SizedBox(
-      height: 60,
-      child: Container(
-          height: 30,
-          margin: const EdgeInsets.only(left: 12, right: 12, top: 30),
-          child: Row(
-            children: [
-              Container(
-                  width: 63,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                  clipBehavior: Clip.antiAlias,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFFE6E6E6),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  )),
-              const Spacer(),
-              GestureDetector(
-                onTap: () {
-                  print('가방 button');
-                  idxRead.setPage(0);
-                },
-                child: const Icon(Icons.work_outline_outlined, size: 28),
+    return Container(
+        height: 30,
+        margin: const EdgeInsets.only(left: 12, right: 12, top: 30),
+        child: Row(
+          children: [
+            SizedBox(
+              width: 63,
+              child: Row(
+                children: [
+                  Container(
+                    width: 21,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/coin.png'))),
+                  ),
+                  const Gap(3),
+                  const Text('num')
+                ],
               ),
-              const SizedBox(width: 19),
-              GestureDetector(
+            ),
+            const Spacer(),
+            GestureDetector(
+              onTap: () {
+                idxRead.setPage(0);
+              },
+              child: Container(
+                width: 28,
+                height: 28,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/calendar_page.png'))),
+              ),
+            ),
+            const SizedBox(width: 16),
+            GestureDetector(
                 onTap: () {
-                  print('사람 button');
                   idxRead.setPage(1);
                 },
-                child: const Icon(Icons.person_outline, size: 28),
-              )
-            ],
-          )),
-    );
+                child: Container(
+                  width: 28,
+                  height: 28,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/my_page.png'))),
+                ))
+          ],
+        ));
   }
 }
