6c0063d58246556ccd1367056d919ce2f47d86f5
links5925 <kilol0@naver.com>
2024-05-07 23:53:13 +0900

    사진 경로 함수화

--- lib/theme/assets.dart
+++ lib/theme/assets.dart
@@ -1,11 +1,22 @@
 class Assets {
   Assets._();
 
-  /* LinkMoa Image */
-  static const image_link = "assets/images/link_image.png";
+  /* Planear Image */
+  static const coin = "assets/icons/coin.png";
+  static const calendar_select = "assets/icons/select_calendar_page.png";
+  static const calendar = "assets/icons/calendar_page.png";
+  static const my_page = "assets/icons/my_page.png";
+  static const my_page_select = "assets/icons/select_my_page.png";
   static const logo = "assets/images/logo.png";
 
-  /* SNS Button Image */
-
-  /* Video Tag Image */
+  /* Avatar Image */
+  static const avatar_accessory = "assets/images/avatar/accessory/";
+  static const avatar_body_root = "assets/images/avatar/body/";
+  static const avatar_guitar = "assets/images/avatar/guitar";
+  static const avatar_face_root = "assets/images/avatar/face/";
+  static const avatar_lower_hair_root = "assets/images/avatar/lower_hair/";
+  static const avatar_pants_root = "assets/images/avatar/pants/";
+  static const avatar_shoes_root = "assets/images/avatar/shoes/";
+  static const avatar_top_root = "assets/images/avatar/top/";
+  static const avatar_upper_hair_root = "assets/images/avatar/upper_hair/";
 }
