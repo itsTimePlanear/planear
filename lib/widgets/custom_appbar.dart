c91a55499463b21afe540afe000b5f9f71133738
links5925 <kilol0@naver.com>
2024-05-07 23:53:48 +0900

    앱바 수정 - 상태에 따른 위젯 변화 추가

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:planear/riverpod/avatar_page_riverpod/avatar_page_riverpod.dart';
 import 'package:planear/riverpod/mainpage_rierpod.dart';
+import 'package:planear/theme/assets.dart';
 
 class MainAppBar extends ConsumerStatefulWidget {
   const MainAppBar({super.key});
@@ -14,7 +16,11 @@ class MainAppBar extends ConsumerStatefulWidget {
 class _MainAppBarState extends ConsumerState<MainAppBar> {
   @override
   Widget build(BuildContext context) {
-    final idxRead = ref.read(bottomChangeStateNotifierProvider.notifier);
+    final mainPageController =
+        ref.read(mainPageChangeStateNotifierProvider.notifier);
+    final avatarPageController =
+        ref.read(avatarPageChangeStateNotifierProvider.notifier);
+    final pageState = ref.watch(mainPageChangeStateNotifierProvider);
     return Container(
         height: 30,
         margin: const EdgeInsets.only(left: 12, right: 12, top: 30),
@@ -28,10 +34,9 @@ class _MainAppBarState extends ConsumerState<MainAppBar> {
                     width: 21,
                     height: 30,
                     decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/coin.png'))),
+                        image: DecorationImage(image: AssetImage(Assets.coin))),
                   ),
-                  const Gap(3),
+                  const Gap(8),
                   const Text('num')
                 ],
               ),
@@ -39,29 +44,40 @@ class _MainAppBarState extends ConsumerState<MainAppBar> {
             const Spacer(),
             GestureDetector(
               onTap: () {
-                idxRead.setPage(0);
+                mainPageController.setPage(0);
               },
               child: Container(
                 width: 28,
                 height: 28,
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                     image: DecorationImage(
-                        image: AssetImage('assets/images/calendar_page.png'))),
+                        image: pageState == 0
+                            ? const AssetImage(Assets.calendar_select)
+                            : const AssetImage(Assets.calendar))),
               ),
             ),
             const SizedBox(width: 16),
-            GestureDetector(
-                onTap: () {
-                  idxRead.setPage(1);
-                },
-                child: Container(
-                  width: 28,
-                  height: 28,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage('assets/images/my_page.png'))),
-                ))
+            _avatarPageButton(
+                mainPageController, avatarPageController, pageState)
           ],
+        ));
+  }
+
+  Widget _avatarPageButton(MainPageChange mainPagecontroller,
+      AvatarPageChange avatarPageController, int pageState) {
+    return GestureDetector(
+        onTap: () {
+          mainPagecontroller.setPage(1);
+          avatarPageController.setPage(AvatarPageState.main);
+        },
+        child: Container(
+          width: 28,
+          height: 28,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: pageState == 1
+                      ? const AssetImage(Assets.my_page_select)
+                      : const AssetImage(Assets.my_page))),
         ));
   }
 }
