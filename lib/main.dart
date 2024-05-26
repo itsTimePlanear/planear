95d572f66bf7c0a4e13b5b667436fffb3bd1d3bc
links5925 <kilol0@naver.com>
2024-05-26 16:05:22 +0900

    calendar 한글 패치 - locale 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:planear/screen/splash_screen/splash_logo_screen.dart';
 
-void main() {
+void main() async {
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -16,6 +17,12 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return const MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('ko')],
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SplashLogo(),
