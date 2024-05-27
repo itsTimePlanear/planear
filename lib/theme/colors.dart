d6f4036b5d2cea2751614d3f0e0b005850156bce
links5925 <kilol0@naver.com>
2024-05-27 22:50:26 +0900

    앱 내 색상 추가

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -14,4 +14,27 @@ class AppColors {
   static const Color grey300 = Color(0xffDFDFDF);
   static const Color grey600 = Color(0xff777777);
   static const Color grey800 = Color(0xff2A2A2A);
+
+  static const Color main_black = Color(0xff111111);
+  static const Color sub_black = Color(0xff767676);
+  static const Color main1 = Color(0xff2f2e2c);
+  static const Color main2 = Color(0xff505050);
+  static const Color main3 = Color(0xfff4f4f4);
+  static const Color delete = Color(0xffdc0000);
+
+  static const Color main_white = Color(0xffffffff);
+  static const Color sub_white = Color(0xff999999);
+
+  /* Custom Color */
+  static const Color color1 = Color(0xff71C5B6);
+  static const Color color2 = Color(0xff1482C1);
+  static const Color color3 = Color(0xffF0B022);
+  static const Color color4 = Color(0xffFB6ABB);
+  static const Color color5 = Color(0xff847BBE);
+
+  static const String str_color1 = '0xff71C5B6';
+  static const String str_color2 = '0xff1482C1';
+  static const String str_color3 = '0xffF0B022';
+  static const String str_color4 = '0xffFB6ABB';
+  static const String str_color5 = '0xff847BBE';
 }
