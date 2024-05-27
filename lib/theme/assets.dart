92f70c22e1bf6c6ebcd7467fab9173ce941d4349
links5925 <kilol0@naver.com>
2024-05-27 22:49:08 +0900

    아바타 파일 분리, 로고 파일 추가 -naming_screen.png 변경 필요

--- lib/theme/assets.dart
+++ lib/theme/assets.dart
@@ -7,16 +7,6 @@ class Assets {
   static const calendar = "assets/icons/calendar_page.png";
   static const my_page = "assets/icons/my_page.png";
   static const my_page_select = "assets/icons/select_my_page.png";
-  static const logo = "assets/images/logo.png";
-
-  /* Avatar Image */
-  static const avatar_accessory = "assets/images/avatar/accessory/";
-  static const avatar_body_root = "assets/images/avatar/body/";
-  static const avatar_etc = "assets/images/avatar/guitar";
-  static const avatar_face_root = "assets/images/avatar/face/";
-  static const avatar_lower_hair_root = "assets/images/avatar/lower_hair/";
-  static const avatar_pants_root = "assets/images/avatar/pants/";
-  static const avatar_shoes_root = "assets/images/avatar/shoes/";
-  static const avatar_top_root = "assets/images/avatar/top/";
-  static const avatar_upper_hair_root = "assets/images/avatar/upper_hair/";
+  static const logo = "assets/icons/planear_logo.png";
+  static const naming_screen = "assets/icons/naming_screen.png";
 }
