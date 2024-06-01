1172f48b79d796bd6fd03758cce88e168e47625c
links5925 <kilol0@naver.com>
2024-06-01 23:53:52 +0900

    schedule 모델 상속

--- lib/screen/calendar_screen/schedule_container.dart
+++ lib/screen/calendar_screen/schedule_container.dart
@@ -3,9 +3,11 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:planear/model/schedule.dart';
 
 class ScheduleContainer extends ConsumerStatefulWidget {
-  const ScheduleContainer({super.key});
+  final Schedule schedule;
+  const ScheduleContainer(this.schedule, {super.key});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
@@ -15,47 +17,39 @@ class ScheduleContainer extends ConsumerStatefulWidget {
 class _ScheduleContainerState extends ConsumerState<ScheduleContainer> {
   @override
   Widget build(BuildContext context) {
+    Schedule schedule = widget.schedule;
     return GestureDetector(
         onTap: () {
           print('object');
         },
-        child: GestureDetector(onTap: () {
-          
-        },
-          child: Container(
-            width: 311,
-            height: 43,
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-            margin: const EdgeInsets.symmetric(vertical: 12),
-            clipBehavior: Clip.antiAlias,
-            decoration: ShapeDecoration(
+        child: Container(
+          width: 311,
+          height: 43,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          margin: const EdgeInsets.symmetric(vertical: 12),
+          clipBehavior: Clip.antiAlias,
+          decoration: ShapeDecoration(
               color: const Color(0xFFF4F4F4),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
-            child: Row(
-              children: [
-                Container(
+                  borderRadius: BorderRadius.circular(12))),
+          child: Row(
+            children: [
+              Container(
                   width: 10,
                   height: 10,
-                  decoration: BoxDecoration(
-                      color: Color.fromRGBO(Random().nextInt(256),
-                          Random().nextInt(256), Random().nextInt(256), 1)),
-                ),
-                const Gap(13),
-                const Text(
-                  maxLines: 1,
-                  '문화 기호와 상상력 수업',
-                  style: TextStyle(
+                  decoration:
+                      BoxDecoration(color: Color(int.parse(schedule.color)))),
+              const Gap(13),
+              Text(
+                maxLines: 1,
+                schedule.name,
+                style: const TextStyle(
                     color: Color(0xFF111111),
                     fontSize: 14,
                     fontFamily: 'Pretendard',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
-            ),
+                    fontWeight: FontWeight.w400),
+              ),
+            ],
           ),
         ));
   }
