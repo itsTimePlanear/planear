a115f6c40520c6d89eda09526bd2ba936aaa913a
links5925 <kilol0@naver.com>
2024-06-25 22:15:07 +0900

    변수 const 추가

--- lib/dummydata/schedule_dummy.dart
+++ lib/dummydata/schedule_dummy.dart
@@ -4,25 +4,25 @@ import 'package:planear/theme/colors.dart';
 Schedule schedule1 = Schedule(
     id: '1111',
     start: DateTime.now(),
-    end: DateTime.now().add(Duration(days: 3)),
+    end: DateTime.now().add(const Duration(days: 3)),
     name: '1번',
     color: AppColors.color1);
 Schedule schedule2 = Schedule(
     id: '2222',
-    start: DateTime.now().add(Duration(days: 2)),
-    end: DateTime.now().add(Duration(days: 4)),
+    start: DateTime.now().add(const Duration(days: 2)),
+    end: DateTime.now().add(const Duration(days: 4)),
     name: '2번',
     color: AppColors.color2);
 Schedule schedule3 = Schedule(
     id: '3333',
-    start: DateTime.now().add(Duration(days: 4)),
-    end: DateTime.now().add(Duration(days: 5)),
+    start: DateTime.now().add(const Duration(days: 4)),
+    end: DateTime.now().add(const Duration(days: 5)),
     name: '3번',
     color: AppColors.color3);
 Schedule schedule4 = Schedule(
     id: '4444',
     start: DateTime.now(),
-    end: DateTime.now().add(Duration(days: 6)),
+    end: DateTime.now().add(const Duration(days: 6)),
     name: '4번',
     color: AppColors.color4);
 Schedule schedule5 = Schedule(
