6933cf85d4add4f9813d84d31e9e1fa8da6fdd96
links5925 <kilol0@naver.com>
2024-06-25 22:30:34 +0900

    api 연결 - 유저 체크, 유저 생성

--- lib/screen/splash_screen/naming_screen.dart
+++ lib/screen/splash_screen/naming_screen.dart
@@ -1,10 +1,17 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:planear/main.dart';
+import 'package:planear/riverpod/name_riverpod.dart';
 import 'package:planear/screen/main_screen/main_screen.dart';
 import 'package:planear/theme/assets.dart';
+import 'package:planear/theme/local_db.dart';
+import 'package:http/http.dart' as http;
+import 'package:planear/theme/url_root.dart';
 
 class NamingScreen extends ConsumerStatefulWidget {
   const NamingScreen({super.key});
@@ -14,6 +21,39 @@ class NamingScreen extends ConsumerStatefulWidget {
 }
 
 class _NamingScreenState extends ConsumerState<NamingScreen> {
+  checkName(String name) async {
+    final url = Uri.parse('${UrlRoot.root}/user/check');
+    final response = await http.post(url,
+        body: jsonEncode({'name': name}),
+        headers: {'Content-Type': 'application/json'});
+    debugPrint('${response.statusCode}');
+    if (response.statusCode == 200) {
+      makeNewName(name);
+    } else if (response.statusCode == 400) {}
+  }
+
+  makeNewName(String name) async {
+    debugPrint(name);
+    final url = Uri.parse('${UrlRoot.root}/user');
+    final response = await http.post(url,
+        body: jsonEncode({'name': name}),
+        headers: {'Content-Type': 'application/json'});
+    if (response.statusCode == 200) {
+      debugPrint(response.body);
+      String id = response.body;
+      ref.read(nameChangeStateNotifierProvider.notifier).setName(name);
+      ref.read(idChangeStateNotifierProvider.notifier).setId(int.parse(id));
+      await storage.write(key: LocalDB.name, value: name);
+      await storage.write(key: LocalDB.id, value: id);
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const MainScreen()));
+    } else {
+      debugPrint('${response.statusCode}');
+    }
+  }
+
+  TextEditingController nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,28 +76,26 @@ class _NamingScreenState extends ConsumerState<NamingScreen> {
               Container(
                   width: 300,
                   height: 48,
-                  padding: EdgeInsets.only(left: 18),
+                  padding: const EdgeInsets.only(left: 18),
                   decoration: ShapeDecoration(
                     color: Colors.white,
                     shape: RoundedRectangleBorder(
-                      side: BorderSide(width: 1, color: Color(0xFFE5E5EC)),
+                      side:
+                          const BorderSide(width: 1, color: Color(0xFFE5E5EC)),
                       borderRadius: BorderRadius.circular(12),
                     ),
                   ),
-                  child: const TextField(
-                    decoration: InputDecoration(
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
                       border: InputBorder.none,
                       hintText: '이름을 지어주세요.',
                     ),
                   )),
               const Spacer(),
               GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const MainScreen(),
-                        ));
+                  onTap: () async {
+                    checkName(nameController.text);
                   },
                   child: Container(
                     width: 343,
