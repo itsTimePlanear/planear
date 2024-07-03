20a2cb99cd79a3bed1962a2d61033dd15c0f8ef0
links5925 <kilolo0@naver.com>
2024-07-03 21:46:48 +0900

    getItem viewmodel 작성

--- lib/viewmodel/item_screen/get_items_view_model.dart
+++ lib/viewmodel/item_screen/get_items_view_model.dart
@@ -2,29 +2,33 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:planear/dummydata/item_dummy.dart';
 import 'package:planear/model/item.dart';
+import 'package:planear/riverpod/item_screen_riverpod/avatar_items_riverpod.dart';
 import 'package:planear/riverpod/user_riverpod.dart';
 import 'package:planear/theme/url_root.dart';
 import 'package:http/http.dart' as http;
 
 Future<void> getItems(WidgetRef ref) async {
   debugPrint('getItems');
-  int category = 1;
-  final url = Uri.parse('${UrlRoot.root}/avatar/store/$category');
-  int id = ref.watch(idChangeStateNotifierProvider);
-  final response = await http.get(url, headers: {'user-no': 16.toString()});
+  ref.read(itemsStateNotifierProvider.notifier).setEmpty();
 
-  if (response.statusCode == 200) {
-    final jsonLists = jsonDecode(response.body);
-    List<dynamic> jsonItems = jsonLists['success']['items'];
-    // for (var element in jsonItems) {
-    //   items.add(Item.fromJson(element));
-    // }
-    List<Item> items = [];
-    items = dummyItems;
-    debugPrint('store api 변경 후 연동 필요');
-  } else {
-    debugPrint(response.statusCode.toString());
+  for (int category = 1; category < 8; category++) {
+    final url = Uri.parse('${UrlRoot.root}/avatar/store/$category');
+    int id = ref.watch(idChangeStateNotifierProvider);
+    final response = await http.get(url, headers: {'user-no': id.toString()});
+
+    if (response.statusCode == 200) {
+      debugPrint('$id items 호출 성공');
+      final jsonLists = jsonDecode(response.body);
+      List<dynamic> jsonItems = jsonLists['success']['items'];
+      List<Item> items = [];
+      for (var jsonItem in jsonItems) {
+        items.add(Item.fromJson(jsonItem, category));
+      }
+      ref.read(itemsStateNotifierProvider.notifier).addItems(items);
+    } else {
+      debugPrint(response.statusCode.toString());
+      break;
+    }
   }
 }
