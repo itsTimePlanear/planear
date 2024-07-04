58e503affdb6ba78683ffe5eb847fee6bd1f0fb0
jeong724 <oliveyou724@gmail.com>
2024-07-04 21:38:26 +0900

    소셜 : 상태메지 업데이트 알림 추가

--- lib/screen/social_screen/social_screen.dart
+++ lib/screen/social_screen/social_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/svg.dart';
@@ -20,23 +21,45 @@ class _SocialScreenState extends ConsumerState<SocialScreen>{
   @override
   Widget build(BuildContext ) {
     return SingleChildScrollView(
-      child: Column(children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-                padding: const EdgeInsets.only(top: 24),
-          child: Column(
-            children: [
-              Text("오늘의 일정 달성률", style: FontStyles.Schedule.copyWith(color: Colors.black)),
-              _avatarCardWidget(true,"assets/icons/avatar2.png", "김춘삼", 50),
-              _avatarMyCardWidget(true,"assets/icons/avatar2.png", "김춘삼", 50),
-            ],
-          ))
-      ],)
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const EdgeInsets.only(top: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("오늘의 일정 달성률", style: FontStyles.Schedule.copyWith(color: Colors.black)),
+            Gap(20),
+           SizedBox( 
+          height: MediaQuery.sizeOf(context).height*0.18,
+           child:  _avatarListWidget(5)),
+           Gap(20),
+           Text("최신 소식", style: FontStyles.Schedule.copyWith(color: Colors.black)),
+            Gap(20),
+             _shareButton()
+
+          ],
+        ))
       );
     
   }
 
-  Widget _avatarCardWidget(bool mine,String prictureUrl, String name, int percent){
+  Widget _avatarListWidget(int count, ){
+
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+       physics: ClampingScrollPhysics(),
+      shrinkWrap: true,
+      itemBuilder: (BuildContext ctx, int idx) {
+        if(idx == 0){
+          return _avatarMyCardWidget("assets/icons/avatar2.png", "김춘삼", 50);
+        } else{
+          return _avatarCardWidget("assets/icons/avatar2.png", "김춘삼", 50);
+        }
+    }, 
+    itemCount: count,);
+  }
+
+  Widget _avatarCardWidget(String prictureUrl, String name, int percent){
 
     return Container(
       width: MediaQuery.sizeOf(context).width*0.3,
@@ -46,6 +69,7 @@ class _SocialScreenState extends ConsumerState<SocialScreen>{
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Gap(8),
            //Image.network(prictureUrl),
            Image.asset(prictureUrl, width: MediaQuery.sizeOf(context).width*0.2, height: MediaQuery.sizeOf(context).height*0.1,),
            Text(name, style: FontStyles.socialName.copyWith(color: Colors.black), ),
@@ -55,9 +79,9 @@ class _SocialScreenState extends ConsumerState<SocialScreen>{
               width: MediaQuery.of(context).size.width * 0.2,
               child: LinearPercentIndicator(
                 animation: true,
-                lineHeight: 10.0,
+                lineHeight: 8.0,
                 animationDuration: 2500,
-                percent: 0.8,
+                percent: 0.5,
                 barRadius: const Radius.circular(10),
                 progressColor: Colors.black,
                 backgroundColor: Color(0xffE5E5EC),
@@ -72,7 +96,7 @@ class _SocialScreenState extends ConsumerState<SocialScreen>{
     );
   }
 
-  Widget _avatarMyCardWidget(bool mine,String prictureUrl, String name, int percent){
+  Widget _avatarMyCardWidget(String prictureUrl, String name, int percent){
 
     return Container(
       width: MediaQuery.sizeOf(context).width*0.3,
@@ -82,6 +106,7 @@ class _SocialScreenState extends ConsumerState<SocialScreen>{
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+             Gap(8),
            //Image.network(prictureUrl),
            Image.asset(prictureUrl, width: MediaQuery.sizeOf(context).width*0.2, height: MediaQuery.sizeOf(context).height*0.1,),
            Text(name, style: FontStyles.socialName.copyWith(color: Colors.white), ),
@@ -91,9 +116,9 @@ class _SocialScreenState extends ConsumerState<SocialScreen>{
               width: MediaQuery.of(context).size.width * 0.2,
               child: LinearPercentIndicator(
                 animation: true,
-                lineHeight: 10.0,
+                lineHeight: 8.0,
                 animationDuration: 2500,
-                percent: 0.8,
+                percent: 0.5,
                 barRadius: const Radius.circular(10),
                 progressColor: Colors.white,
                 backgroundColor: AppColors.main2,
@@ -106,6 +131,33 @@ class _SocialScreenState extends ConsumerState<SocialScreen>{
         ],),
       ),
     );
+  }
+
+  Widget _shareButton(){
+
+    return Row(
+    children: [
+      GestureDetector(
+        onTap: (){
+          
+        },
+        child: Container(
+          child: Stack(
+            children: [
+              Container(
+               padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 8),
+          
+                width: MediaQuery.sizeOf(context).width-50,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white),
+              child: Text("상태 메세지를 업데이트 해 친구들에게 공유하세요.", style: FontStyles.Week.copyWith(color: Colors.black),),
+            ),
+              
+          Positioned(child: SvgPicture.asset("assets/icons/social_pencil.svg"),
+          )]),
+        ),
+      )
+    ],
+          );
   }
 
 }
