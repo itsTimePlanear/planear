31169f5fff4738a7488573dbf9231acbd80f0ff4
links5925 <kilol0@naver.com>
2024-07-08 23:53:54 +0900

    미사용 png 삭제

--- lib/theme/assets.dart
+++ lib/theme/assets.dart
@@ -4,9 +4,6 @@ class Assets {
   /* Planear Image */
   static const coin = "assets/icons/coin.png";
   static const calendar_select = "assets/icons/select_calendar_page.png";
-  static const calendar = "assets/icons/calendar_page.png";
-  static const my_page = "assets/icons/my_page.png";
-  static const my_page_select = "assets/icons/select_my_page.png";
   static const logo = "assets/icons/planear_logo.png";
   static const naming_screen = "assets/icons/naming_screen.png";
   static const appbar_bell = "assets/icons/appbar_bell.svg";
