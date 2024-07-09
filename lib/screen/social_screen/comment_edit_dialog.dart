731860be0d3301fed773eb196e97082da8597134
jeong724 <oliveyou724@gmail.com>
2024-07-09 17:41:58 +0900

    상태메세지 변경 ui

--- lib/screen/social_screen/comment_edit_dialog.dart
+++ lib/screen/social_screen/comment_edit_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:planear/theme/font_styles.dart';
 import 'package:planear/widgets/state_message_percent.dart';
 import 'package:planear/widgets/state_message_qa.dart';
 import 'package:planear/widgets/state_message_todo.dart';
@@ -11,21 +13,30 @@ Future<bool> showCommentEditDialog(
     builder: (BuildContext context){
 
       return Dialog(
-        alignment: Alignment.center,
+        alignment: Alignment.topCenter,
+        backgroundColor: Colors.transparent,
         insetPadding: const EdgeInsets.symmetric(
             horizontal: 25,
           ),
-        child : Container(
-          height: 150,
-          child: PageView(
-            children: [
-              StateMessagePercent(),
-              StateMessageTodo(),
-              StateMessageQa(),
-            ],
-          ),
+        child : Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Gap(30),
+            Text("상태메세지", style: FontStyles.Title.copyWith(color: Colors.white),),
+            Gap(50),
+            Container(
+              height: 150,
+              child: PageView(
+                children: [
+                  StateMessagePercent(),
+                  StateMessageTodo(),
+                  StateMessageQa(),
+                ],
+              ),
+            ),
+            Gap(30),
+          ],
         ),
-        
       );
     });
     return result;
