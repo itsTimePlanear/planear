5cd1776bc9320a24cbb787edec7abd628bdf83ff
links5925 <kilol0@naver.com>
2024-07-18 03:42:25 +0900

    초기 유저 id 생성 페이지 디테일 추가

--- lib/screen/splash_screen/naming_screen.dart
+++ lib/screen/splash_screen/naming_screen.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:planear/riverpod/splash_screen_riverpod/splash.dart';
 import 'package:planear/screen/main_screen/main_screen.dart';
 import 'package:planear/theme/assets.dart';
 import 'package:planear/repository/splash_screen/naming_screen_repo.dart';
+import 'package:planear/widgets/custom_dialog.dart';
 
 class NamingScreen extends ConsumerStatefulWidget {
   const NamingScreen({super.key});
@@ -16,11 +18,18 @@ class NamingScreen extends ConsumerStatefulWidget {
 
 class _NamingScreenState extends ConsumerState<NamingScreen> {
   TextEditingController nameController = TextEditingController();
+  FocusNode nameFocus = FocusNode();
   @override
   Widget build(BuildContext context) {
+    bool focus = ref.watch(nameFocusProvider);
+    SplashFocusChange focusController = ref.read(nameFocusProvider.notifier);
+    bool duplication = ref.watch(nameDuplicationProvider);
+    SplashNameCheck duplicationController =
+        ref.read(nameDuplicationProvider.notifier);
     return Scaffold(
       backgroundColor: const Color(0xffF4F4F4),
       body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
         child: SizedBox(
           width: MediaQuery.sizeOf(context).width,
           height: MediaQuery.sizeOf(context).height,
@@ -35,35 +44,83 @@ class _NamingScreenState extends ConsumerState<NamingScreen> {
                         image: AssetImage(Assets.naming_screen))),
               ),
               const Gap(10),
-              Container(
-                  width: 300,
-                  height: 48,
-                  padding: const EdgeInsets.only(left: 18),
-                  decoration: ShapeDecoration(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      side:
-                          const BorderSide(width: 1, color: Color(0xFFE5E5EC)),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: '이름을 지어주세요.',
-                    ),
-                  )),
+              Column(
+                crossAxisAlignment: duplication
+                    ? CrossAxisAlignment.start
+                    : CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 300,
+                      height: 48,
+                      padding: const EdgeInsets.only(left: 18),
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              width: 1,
+                              color: focus
+                                  ? const Color(0xFF111111)
+                                  : duplication
+                                      ? const Color(0xFFDC0000)
+                                      : const Color(0xFFE5E5EC)),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: TextField(
+                        onTap: () {
+                          focusController.setTrue();
+                        },
+                        onTapOutside: (_) {
+                          focusController.setFalse();
+                          nameFocus.unfocus();
+                        },
+                        onEditingComplete: () {
+                          focusController.setFalse();
+                          nameFocus.unfocus();
+                        },
+                        onSubmitted: (_) {
+                          focusController.setFalse();
+                          nameFocus.unfocus();
+                        },
+                        controller: nameController,
+                        focusNode: nameFocus,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: '이름을 지어주세요.',
+                        ),
+                      )),
+                  const Gap(10),
+                  duplication
+                      ? const Text(
+                          ' 이미 사용중인 이름입니다.',
+                          style: TextStyle(
+                            color: Color(0xFFDC0000),
+                            fontSize: 14,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: -0.35,
+                          ),
+                        )
+                      : Container()
+                ],
+              ),
               const Spacer(),
               GestureDetector(
                   onTap: () async {
-                    if (await checkName(nameController.text)) {
-                      if (await makeNewName(
-                          context, nameController.text, ref)) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const MainScreen()));
+                    if (nameController.text.isNotEmpty) {
+                      if (await showCustomDialog(
+                          context, '아바타 생성을 완료할까요?', '취소', '완료하기')) {
+                        if (await checkName(nameController.text)) {
+                          if (await makeNewName(
+                              context, nameController.text, ref)) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const MainScreen()));
+                          }
+                        } else {
+                          duplicationController.setTrue();
+                        }
                       }
                     }
                   },
