247de35a67d034d69f4511a03f35f1788390679c
jeong724 <oliveyou724@gmail.com>
2024-07-23 13:00:04 +0900

    상태 메세지 버튼 수정

--- lib/screen/social_screen/social_screen.dart
+++ lib/screen/social_screen/social_screen.dart
@@ -242,7 +242,7 @@ class _SocialScreenState extends ConsumerState<SocialScreen> {
         Container(
           child: Stack(children: [
             Container(
-              padding: const EdgeInsets.only(right: 60, top: 8,bottom: 8 ,left: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 8),
               width: MediaQuery.sizeOf(context).width - 50,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10), color: Colors.white),
